20dd9a3ef80f6bf6e747467b43b1445cfe15f306
Claudina Cortez <claudinacr@gmail.com>
2018-01-25 22:25:36 -0300

    Fix de exportacion de modulo extract_links

--- test/test.js
+++ test/test.js
@@ -1,16 +1,1 @@
-var extract_links = require('./lib/extract_links');
-
-//const isVowel = require('./isVowel');
-
-describe('Extractor de links', () => {
-    
-  it('debería devolver true para letra a', () => {
-    extract_links.extract()
-    // Acá invocamos `isVowel()` y verificamos el resultado
-  });
-
-  it('debería devolver false para letra b', () => {
-    // Acá invocamos `isVowel()` y verificamos el resultado
-  });
-
-});
+var extract_links = require('../lib/extract_links');
