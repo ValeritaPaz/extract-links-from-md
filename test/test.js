c28f33f964e707f0295410324ce08c91533b6608
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-02-02 08:33:30 -0300

    Mejora del Demo

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 // let/const
 // Archivo de Prueba para test con mocha
-var extract_links = require('../lib/extract_links');
+let extractLinks = require('../lib/extract_links');
 
-var str = `# Lyft
+let str = `# Lyft
 
 * **Track:** _Common Core_
 * **Curso:** _Creando tu primer sitio web interactivo_
@@ -19,7 +19,8 @@ estructura de carpetas y archivos donde colocarás tu código.
 1. Debes realizar un [**fork**](https://gist.github.com/ivandevp/1de47ae69a5e139a6622d78c882e1f74)
    de este repositorio.
 
-2. Luego deberás **clonar** tu fork en tu máquina. Recuerda que el comando a usar
+2. Luego deberás **clonar** tu fork en tu máquina. Recuerda que el 
+comando a usar
    es \`git clone\` y su estructura normalmente se ve así:
 
    \`\`\`bash
@@ -95,11 +96,7 @@ Este reto será evaluado sobre lo siguiente:
 * Archivo README.md actualizado y correctamente redactado
 * Uso de comentarios para hacer tu código más legible`;
 
-var resultado = extract_links.extract(str);
-
-//console.log(resultado);
-
-var str2 = `# Freelancer
+let str2 = `# Freelancer
 
 * **Track:** _Common Core_
 * **Curso:** _Creando tu primer sitio web interactivo_
@@ -116,7 +113,8 @@ estructura de carpetas y archivos donde colocarás tu código.
 1. Debes realizar un [**fork**](https://gist.github.com/ivandevp/1de47ae69a5e139a6622d78c882e1f74)
    de este repositorio.
 
-2. Luego deberás **clonar** tu fork en tu máquina. Recuerda que el comando a usar
+2. Luego deberás **clonar** tu fork en tu máquina. 
+Recuerda que el comando a usar
    es \`git clone\` y su estructura normalmente se ve así:
 
    \`\`\`bash
@@ -180,7 +178,7 @@ Este reto será evaluado sobre lo siguiente:
 * Archivo \`README.md\` actualizado y correctamente redactado
 * Uso de comentarios para hacer tu código más legible`;
 
-var str3 = `# User Experience Design
+let str3 = `# User Experience Design
 
 Este repositorio es el lugar donde vamos a ir continuamente _diseñando_ nuestro
 programa de formación en **User Experience Design**.
@@ -197,12 +195,17 @@ Este mapa es un mapa vivo, se basa en feedback de empleadores, instructores y
 alumnas. Si tienes alguna sugerencia o comentario no dudes en hacerlo. 
 
 
-Además del feedback de empleadores, instructores y alumnas. Hemos tenido en cuenta comentarios y artítuclos de referentes en el diseño. Aquí algunas de nuestras referencias:
+Además del feedback de empleadores, instructores y alumnas. 
+Hemos tenido en cuenta comentarios y artítuclos de referentes 
+en el diseño. Aquí algunas de nuestras referencias:
 
 
 ### [Let’s stop talking about THE design process](https://medium.com/stanford-d-school/lets-stop-talking-about-the-design-process-7446e52c13e8)
 
-En este artículo del dschool the la Universidad de Stanford, la autora nos habla de cómo debemos dejar de enfocarnos en "EL" proceso de diseño sino que debemos enfocarnos en determinados skills que deben tener los diseñadores como: 
+En este artículo del dschool the la Universidad de Stanford, 
+la autora nos habla de cómo debemos dejar de enfocarnos en "EL" 
+proceso de diseño sino que debemos enfocarnos en determinados 
+skills que deben tener los diseñadores como: 
 
 * Navegar la ambigüedad
 * Aprender de otros
@@ -217,24 +220,39 @@ Toodos estas habilidades están siendo añadidas en nuestra currícula.
 
 ### [InVision’s Aarron Walter on design culture](https://blog.intercom.com/invisions-aarron-walter-on-design-culture/)
 
-Aarron Walter de Invision nos habla sobre el balance entre teoría y práctica al momento de educar en diseño.
+Aarron Walter de Invision nos habla sobre el balance entre teoría y 
 
-> If you’re entirely academic, but you have not actually practiced or applied those ivory tower ideas, if you haven’t applied those to something practical, then is that very useful? Conversely, if you only know the tools of the trade, can you think very deeply about the meaning of the product and the strategy? Can you think very high level? Maybe not. A balance between those two puts you in a really strong position to be a leader in design and technology, versus just a technician.
+práctica al momento de educar en diseño.
+
+> If you’re entirely academic, but you have not actually practiced 
+or applied those ivory tower ideas, if you haven’t applied those to 
+something practical, then is that very useful? Conversely, if you 
+only know the tools of the trade, can you think very deeply about 
+the meaning of the product and the strategy? Can you think very high 
+level? Maybe not. A balance between those two puts you in a really 
+strong position to be a leader in design and technology, versus just 
+a technician.
 
 ### [Judy Wert - The one thing all top designers have](https://www.youtube.com/watch?v=xPmBRhMX8qQ)
 
-En esta entrevista, Judy Wert, nos habla de que el top 1% de diseñadores en el mundo saben sobre la historia del diseño y lo complicado que ha sido llegar al rol que juega el diseño hoy en día. Esto está muy relacionado con tener empatía con las personas con quiénes trabajamos. 
+En esta entrevista, Judy Wert, nos habla de que el top 1% de diseñadores 
+en el mundo saben sobre la historia del diseño y lo complicado que ha 
+sido llegar al rol que juega el diseño hoy en día. Esto está muy 
+relacionado con tener empatía con las personas con quiénes trabajamos. 
 
 ### [Jared Spool - How to hire designers](https://blog.intercom.com/jared-spool-on-ux-design/)
 
-En esta entrevista, Jared Spool, co-founder de Center Centre, explica cómo es la mejor manera de contratar un diseñador. Y cuestiona la relación entre seniority y años de experiencia. 
+En esta entrevista, Jared Spool, co-founder de Center Centre, 
+explica cómo es la mejor manera de contratar un diseñador. 
+Y cuestiona la relación entre seniority y años de experiencia. 
 
 ### [Daniel Burka - Design is the scientific method of businesses, if done right](https://www.youtube.com/watch?v=t44kmZ_PFOU)
 
 
 ### [Paul Sherman - The unicorn is dead. Soft skills kill coding skills](https://www.youtube.com/watch?v=tSkDLYDsbBc)
 
-En este talk Paul comenta sobre la importancia de los soft skills para un UX Designer. Y destaca los siguientes:
+En este talk Paul comenta sobre la importancia de los soft skills para 
+un UX Designer. Y destaca los siguientes:
 
 * Comunicación e influencia
 * Colaboración
@@ -252,13 +270,20 @@ En este talk Paul comenta sobre la importancia de los soft skills para un UX Des
 * Autor: [Lalo Gonzalez](https://github.com/lalogf)
 * Tiempo estimado: _2 semanas_
 
-En este curso trabajaremos en nuestro primer proyecto de diseño, aprendiendo las mejores prácticas de research cualitativo, pasando por técnicas de síntesis, ideación y priorización hasta llegar al prototipado y testeo de la solución. 
+En este curso trabajaremos en nuestro primer proyecto de diseño, 
+aprendiendo las mejores prácticas de research cualitativo, pasando 
+por técnicas de síntesis, ideación y priorización hasta llegar al 
+prototipado y testeo de la solución. 
 
-* **Venta de pasajes de bus**: El entregable final del curso será un prototipo validado de una aplicación de venta de pasajes de bus. 
+* **Venta de pasajes de bus**: El entregable final del curso será 
+un prototipo validado de una aplicación de venta de pasajes de bus. 
 
-Main tags: \`research\`, \`entrevistas\`, \`observación\`, \`prototipado\`, \`mvp\`, \`testing\`, \`priorizacion\`, \`content prototyping\`, \`customer journey maps\`, \`user personas\`.
+Main tags: \`research\`, \`entrevistas\`, \`observación\`, 
+\`prototipado\`, \`mvp\`, \`testing\`, \`priorizacion\`, 
+\`content prototyping\`, \`customer journey maps\`, \`user personas\`.
 
-Secondary tags: \`sketching\`, \`paper prototyping\`, \`content prototyping\`, \`interactive prototype\`,
+Secondary tags: \`sketching\`, \`paper prototyping\`, 
+\`content prototyping\`, \`interactive prototype\`,
 \`investigación contextual\`, \`encuestas\`, \`Figma\`, \`Marvel\`.
 
 *** 
@@ -272,101 +297,97 @@ Pero **POR FAVOR** no olvides leer detalladamente la
 
 ## Licencia & Copyright
 
-Todos los materiales de este repo son (c) 2017 [Laboratoria](http;//laboratoria.la/).
+Todos los materiales de este repo son (c) 2017 [Laboratoria]
+(http;//laboratoria.la/).
 
 [![Creative Commons License](https://i.creativecommons.org/l/by-sa/4.0/88x31.png)](http://creativecommons.org/licenses/by-nc-nd/4.0/)
 
 
 Este trabajo está publicado bajo la licencia [Creative commons Attribution-ShareAlike 4.0](https://creativecommons.org/licenses/by-sa/4.0/).`;
 
-var str4 = `
+let str4 = `
 * Track: _UX Design
 * Tiempo estimado: _2 semanas_
 
-En este curso trabajaremos en nuestro primer proyecto de diseño, aprendiendo las mejores prácticas de research cualitativo, pasando por técnicas de síntesis, ideación y priorización hasta llegar al prototipado y testeo de la solución. 
+En este curso trabajaremos en nuestro primer proyecto de diseño,
+// aprendiendo las mejores prácticas de research cualitativo, pasando 
+// por técnicas de síntesis, ideación y priorización hasta llegar al 
+// prototipado y testeo de la solución. 
 
-* **Venta de pasajes de bus**: El entregable final del curso será un prototipo validado de una aplicación de venta de pasajes de bus. 
+* **Venta de pasajes de bus**: El entregable final del curso será un 
+prototipo validado de una aplicación de venta de pasajes de bus. 
 
-Main tags: \`research\`, \`entrevistas\`, \`observación\`, \`prototipado\`, \`mvp\`, \`testing\`, \`priorizacion\`, \`content prototyping\`, \`customer journey maps\`, \`user personas\`.`
+Main tags: \`research\`, \`entrevistas\`, \`observación\`, \`prototipado\`,
+ \`mvp\`, \`testing\`, \`priorizacion\`, \`content prototyping\`,
+  \`customer journey maps\`, \`user personas\`.`;
 
 
-describe("Prueba de extract()", ()=>{
-
+describe('Prueba de extract()', ()=>{
   it('Debería Devolver true si el string posee enlaces []()', () => {
-    if (typeof extract_links.extract(str) !== 'object') {
+    if (typeof extractLinks.extract(str) !== 'object') {
       console.error('✗ fail');
     } else {
       console.log('✓ ok');
-    } 
+    }
   });
-
-  it('Debería Devolver false si el string No posee enlaces []() e indicar No Links', () => {
-    if ( typeof extract_links.extract(str4) !== 'string') {
+  it('Devuelve false si el string No posee enlaces []()y No Links', () => {
+    if ( typeof extractLinks.extract(str4) !== 'string') {
       console.error('✗ fail');
     } else {
-      console.log('✓ ok ' + extract_links.extract(str4));
-    } 
+      console.log('✓ ok ' + extractLinks.extract(str4));
+    }
   });
+});
 
-})
-
-describe("Prueba de hasLinks()", ()=>{
-
+describe('Prueba de hasLinks()', ()=>{
   it('Debería Devolver true si el string posee enlaces []()', () => {
-    if (extract_links.hasLinks(str) !== true) {
+    if (extractLinks.hasLinks(str) !== true) {
       console.error('✗ fail');
     } else {
       console.log('✓ ok');
-    } 
+    };
   });
 
   it('Debería Devolver false si el string No posee enlaces []()', () => {
-    if (extract_links.hasLinks(str4) !== false) {
+    if (extractLinks.hasLinks(str4) !== false) {
       console.error('✗ fail');
     } else {
       console.log('✓ ok');
-    } 
+    }
   });
+});
 
-})
-
-describe("Prueba de sameSize()", ()=>{
-
+describe('Prueba de sameSize()', ()=>{
   it('Debería Devolver true si los array son de Igual Tamaño', () => {
-    if (extract_links.sameSize([1,23,'d'],['ee',{},11]) !== true) {
+    if (extractLinks.sameSize([1, 23, 'd'], ['ee', {}, 11]) !== true) {
       console.error('✗ fail');
     } else {
       console.log('✓ ok');
-    } 
+    };
   });
-
   it('Debería Devolver false si los array NO son de Igual Tamaño', () => {
-    if (extract_links.sameSize([1,23],['ee',{},11]) !== false) {
+    if (extractLinks.sameSize([1, 23], ['ee', {}, 11]) !== false) {
       console.error('✗ fail');
     } else {
       console.log('✓ ok');
-    } 
+    }
   });
+});
 
-})
-
-describe("Prueba de extractBracketsParentheses()", ()=>{
-
+describe('Prueba de extractBracketsParentheses()', ()=>{
   it('Debería Devolver true si los array son de Igual Tamaño', () => {
-    if (extract_links.sameSize([1,23,'d'],['ee',{},11]) !== true) {
+    if (extractLinks.sameSize([1, 23, 'd'], ['ee', {}, 11]) !== true) {
       console.error('✗ fail');
     } else {
       console.log('✓ ok');
-    } 
+    }
   });
-
   it('Debería Devolver false si los array NO son de Igual Tamaño', () => {
-    if (extract_links.sameSize([1,23],['ee',{},11]) !== false) {
+    if (extractLinks.sameSize([1, 23], ['ee', {}, 11]) !== false) {
       console.error('✗ fail');
     } else {
       console.log('✓ ok');
-    } 
+    }
   });
-
-})
+});
 
